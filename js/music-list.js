dfbf75579acf54e64693dea85e325a184e43781e
Pranjal Sengar <99082414+pranjal2210@users.noreply.github.com>
2022-07-14 20:59:32 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,163 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Khairiyat",
+    artist: "Arijit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Pasoori",
+    artist: "Ali Sethi & Shae Gill",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Duniyaa",
+    artist: "Akhil & Dhvani Bhanushali",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Soch Na Sake",
+    artist: "Arijit Singh & Amaal Mallik",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Roke Na Ruke Naina",
+    artist: "Arijit Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Bekhayali",
+    artist: "Sachet Tandon",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Chale Aana",
+    artist: "Armaan Malik & Amaal Malik",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Hawayein",
+    artist: "Arijit Singh",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Lo Safar",
+    artist: "Jubin Nautiyal",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Hasi",
+    artist: "Ami Mishra",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "Phir Bhi Tumko Chaahunga",
+    artist: "Arijit Singh & Shashaa Tirupati",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Ae Dil Hai Mushkil",
+    artist: "Arijit Singh",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Humsafar",
+    artist: "Akhil Sachdeva",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Tum Hi Ho",
+    artist: "Arijit Singh",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Mann Mera",
+    artist: "Gajendra Verma",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Tera Hone Laga Hoon",
+    artist: "Atif Aslam",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Kabira",
+    artist: "Arijit Singh",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Samjhawan",
+    artist: "Arijit Singh & Shreya Ghoshal",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Agar Tum Sath Ho",
+    artist: "Alka Yagnik & Arijit Singh",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Baaraat",
+    artist: "Ritviz & Nucleya",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Kahani",
+    artist: "Mohan Kannan",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Excuses",
+    artist: "AP Dhillon",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Insane",
+    artist: "AP Dhillon",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "No Love",
+    artist: "Shubh",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "We Rollin",
+    artist: "Shubh",
+    img: "music-24",
+    src: "music-24"
+  },
+];
